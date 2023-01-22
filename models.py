93ec78c76d698068b80e0ddc0d0cce653f44a84d
aykhan-sh <a.turlasov@gmail.com>
2023-01-22 13:20:30 +0000

    to wait new data

--- models.py
+++ models.py
@@ -4,6 +4,6 @@ _models_list = ["CCMat/ddpm-bored-apes-128", "google/ddpm-celebahq-256"]
 
 
 def get_models(imsize: int):
-    unet = UNet2DModel.from_pretrained("google/ddpm-celebahq-256")
+    unet = UNet2DModel.from_pretrained("results_128/weights/unet")
     unet.sample_size = imsize
     return unet
